cb3134dcaacf1953cac6c32b09eababb17fccffe
Elillos <elia.kobe@hotmail.it>
2017-02-28 15:11:41 +0100

    Breve esempio di chat real time con SignalR

---
+++ SignalRChat/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRChat
+{
+    public class ChatHub : Hub
+    {
+        //Metodo che viene invocato dal proxy Hub del client
+        public void Send(string name, string message)
+        {
+            //call the broadcastMessage to update the client
+            //metodo client che viene invocato dal server hub
+            Clients.All.broadcastMessage(name, message);
+        }
+    }
+}
---
+++ SignalRChat/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.AspNet.SignalR;
+
+[assembly: OwinStartup(typeof(SignalRChat.Startup))]
+
+namespace SignalRChat
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            // Tutte le connessioni o le configurazioni dell'hub devono andare in questa classe
+
+            app.MapSignalR();
+
+        }
+    }
+}
